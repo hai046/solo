ac780fb967a10b558c1dcb511e10fc2e3a162483
hai046 <haizhu12345@gmail.com>
2017-07-12 13:59:52 +0800

    update

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+result=`ps -ef|grep -v grep |grep org.b3log.solo.Starter`
+
+echo result
+if [[ result != "" ]];then
+    echo "存在该线程  不能开启"
+    exit 0;
+
+fi
+
+
+mvn clean package  -Dmaven.test.skip=true
+
+rm -rf webapp
+
+mkdir webapp
+
+cp target/solo.war webapp/
+
+cd webapp
+
+unzip solo.war
+
+
+nohup java -cp WEB-INF/lib/*:WEB-INF/classes org.b3log.solo.Starter  &
